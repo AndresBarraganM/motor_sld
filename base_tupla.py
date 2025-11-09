a3aefea849d4c4e229eb02831ed8cdcca394af52
Andres Barragan <al22760549@ite.edu.mx>
2025-11-08 22:16:18 -0800

    tupla para ejercicio de tuplas

---
+++ base_tupla.py
@@ -0,0 +1,115 @@
+hechos = (
+    (
+      "Ácaro del ciclamino",
+      (
+          ("sintoma_hoja_nueva", "deformadas_arrugadas"),
+          ("sintoma_planta", "crecimiento_detenido"),
+          ("sintoma_hoja", "amarillentas"),
+          ("sintoma_fruto", "pequenos_deformes")
+      )
+    ),
+    (
+      "Tijerilla europea",
+      (
+        ("sintoma_fruto", "agujeros_profundos"),
+        ("presencia_insecto", "tijerillas_dentro_fruto")
+      )
+    ),
+    (
+       "Ciempiés de jardín",
+       (
+        ( "presencia_insecto",  "ciempes_en_cerca_planta"),
+        ( "sintoma_raiz",  "dano"),
+        ( "sintoma_fruto_cerca_suelo",  "mordidas_redondas_pequenas")
+      ),
+    ),
+    (
+       "Palomilla tortrix del jardín",
+       (
+        ( "presencia_insecto",  "larvas_verdes_amarillas_cabeza_marron_fruta"),
+        ( "sintoma_hoja",  "enrolladas_torcidas")
+      ),
+    ),
+    (
+       "Chinche lygus",
+       (
+        ( "sintoma_fruto",  "deformes"),
+        ( "sintoma_hoja",  "amarillentas"),
+        ( "presencia_insecto",  "chinches_verde_rojo_naranja")
+      ),
+    ),
+    (
+       "Escarabajos de la raíz",
+       (
+        ( "sintoma_planta",  "baja_normal"),
+        ( "sintoma_hoja",  "amarillentas_marchitas"),
+        ( "sintoma_planta_joven",  "muerte"),
+        ( "sintoma_fruto",  "pequenos_deformes")
+      ),
+    ),
+    (
+       "Gusano peludo",
+       (
+        ( "presencia_insecto",  "insecto_debajo_plantas_dia"),
+        ( "presencia_insecto",  "oruga_pelos_verde_marron_negro"),
+        ( "sintoma_hoja",  "mordidos_maltratados_pelos"),
+        ( "sintoma_fruto",  "mordidos_maltratados_pelos")
+      ),
+    ),
+    (
+       "Xanthomonas fragariae (Mancha angular de la hoja)",
+       (
+        ( "sintoma_lesion",  "pequenas"),
+        ( "sintoma_caliz",  "marron")
+      ),
+    ),
+    (
+       "Rhodococcus fascians (Fasciación)",
+       (
+        ( "sintoma_fresa",  "malformada"),
+        ( "sintoma_hoja",  "islas_verdes")
+      ),
+
+    ),
+    (
+       "Pseudomonas syringae (Tizón bacterial)",
+       (
+        ( "sintoma_corona",  "lesiones"),
+        ( "sintoma_planta",  "marchitamiento"),
+        ( "sintoma_interno",  "decoloracion_interna")
+      ),
+
+    ),
+    (
+       "Frankliniella occidentalis (Thrips occidental de las flores)",
+       (
+        ( "sintoma_flor",  "danos_base"),
+        ( "sintoma_general",  "manchas_pardas")
+      ),
+    ),
+    (
+       "Geotrichum candidum (Pudrición agria)",
+       (
+        ( "sintoma_general",  "olor_agrio"),
+        ( "sintoma_superficie",  "blanco"),
+        ( "sintoma_pudricion",  "empapada")
+      ),
+
+    ),
+    (
+       "Neopestalotiopsis (Mancha foliar y pudrición de la fruta)",
+       (
+        ( "sintoma_hoja",  "manchas_marron"),
+        ( "sintoma_fruto",  "pudricion_negro")
+      ),
+
+    ),
+    (
+
+       "Ramularia tulasnei (Mancha foliar blanca)",
+       (
+        ( "sintoma_hoja",  "manchas_circulares_purpura"),
+        ( "sintoma_hoja",  "centro_blanco")
+      ),
+    )
+)
